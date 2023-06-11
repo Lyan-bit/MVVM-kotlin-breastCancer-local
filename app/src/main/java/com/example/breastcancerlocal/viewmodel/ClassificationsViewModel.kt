0cfdc64f7b12b1ad0e96557f8963b6d4e7497ff1
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 15:41:49 +0100

    Update ClassificationsViewModel.kt

--- app/src/main/java/com/example/breastcancerlocal/viewmodel/ClassificationsViewModel.kt
+++ app/src/main/java/com/example/breastcancerlocal/viewmodel/ClassificationsViewModel.kt
@@ -1,84 +1,87 @@
-package com.example.breastcancerlocal.viewmodel
+package com.example.breastcancerlocal.adapter
 
-import android.content.Context
-import android.content.res.AssetManager
-import androidx.lifecycle.ViewModel
-import com.example.breastcancerlocal.model.BreastCancer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.breastcancerlocal.R
+import com.example.breastcancerlocal.fragments.ListFragment
 import com.example.breastcancerlocal.model.BreastCancerVO
-import org.tensorflow.lite.Interpreter
-import java.io.FileInputStream
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.channels.FileChannel
 
-class ClassificationsViewModel (context: Context): ViewModel() {
+class ClassificationRecyclerViewAdapter(items: List<BreastCancerVO>, listener: ListFragment.OnListFragmentInteractionListener)
+    : RecyclerView.Adapter<ClassificationRecyclerViewAdapter.ViewHolder>() {
 
-    private val assetManager: AssetManager = context.assets
-    private val crudViewModel = BreastCancerViewModel.getInstance(context)
+    private var mValues: List<BreastCancerVO> = items
+    private var mListener: ListFragment.OnListFragmentInteractionListener = listener
 
-    companion object {
-        private var instance: ClassificationsViewModel? = null
-        fun getInstance(context: Context): ClassificationsViewModel {
-            return instance ?: ClassificationsViewModel(context)
+    // Returns a new ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.listrowitems_layout, parent,
+            false)
+        return ViewHolder(itemView)
+    }
+
+    // Displays data at a certain position
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mItem = mValues[position]
+        holder.listClassificationoneView.text = " " + mValues[position].id + " "
+        holder.listClassificationtwoView.text = " " + mValues[position].age + " "
+        holder.listClassificationthreeView.text = " " + mValues[position].bmi + " "
+        holder.listClassificationfourView.text = " " + mValues[position].glucose + " "
+        holder.listClassificationfiveView.text = " " + mValues[position].insulin + " "
+        holder.listClassificationsixView.text = " " + mValues[position].homa + " "
+        holder.listClassificationsevenView.text = " " + mValues[position].leptin + " "
+        holder.listClassificationeightView.text = " " + mValues[position].adiponectin + " "
+        holder.listClassificationnineView.text = " " + mValues[position].resistin + " "
+        holder.listClassificationtenView.text = " " + mValues[position].mcp + " "
+        holder.listClassificationresultView.text = " " + mValues[position].outcome + " "
+
+        holder.mView.setOnClickListener {
+            mListener?.onListFragmentInteraction(holder.mItem)
         }
     }
 
-    //classification
-    private fun loadModelFile(assetManager: AssetManager, modelPath: String): ByteBuffer {
-        val fileDescriptor = assetManager.openFd(modelPath)
-        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
-        val fileChannel = inputStream.channel
-        val startOffset = fileDescriptor.startOffset
-        val declaredLength = fileDescriptor.declaredLength
-        return fileChannel.map(
-            FileChannel.MapMode.READ_ONLY,
-            startOffset, declaredLength)
+    // Returns size of data list
+    override fun getItemCount(): Int {
+        return mValues.size
     }
 
-    fun classify(classification: BreastCancer) : String {
-        var res : String
-        lateinit var tflite : Interpreter
-        lateinit var tflitemodel : ByteBuffer
+    // Describes an item view and its place within the RecyclerView
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        try{
-            tflitemodel = loadModelFile(assetManager, "cancer.tflite")
-            tflite = Interpreter(tflitemodel)
-        } catch(ex: Exception){
-            ex.printStackTrace()
+        var mView: View
+        var listClassificationoneView: TextView
+        var listClassificationtwoView: TextView
+        var listClassificationthreeView: TextView
+        var listClassificationfourView: TextView
+        var listClassificationfiveView: TextView
+        var listClassificationsixView: TextView
+        var listClassificationsevenView: TextView
+        var listClassificationeightView: TextView
+        var listClassificationnineView: TextView
+        var listClassificationtenView: TextView
+        var listClassificationresultView: TextView
+        lateinit var mItem: BreastCancerVO
+
+        init {
+            mView = view
+            listClassificationoneView = view.findViewById(R.id.listClassificationoneView)
+            listClassificationtwoView = view.findViewById(R.id.listClassificationtwoView)
+            listClassificationthreeView = view.findViewById(R.id.listClassificationthreeView)
+            listClassificationfourView = view.findViewById(R.id.listClassificationfourView)
+            listClassificationfiveView = view.findViewById(R.id.listClassificationfiveView)
+            listClassificationsixView = view.findViewById(R.id.listClassificationsixView)
+            listClassificationsevenView = view.findViewById(R.id.listClassificationsevenView)
+            listClassificationeightView = view.findViewById(R.id.listClassificationeightView)
+            listClassificationnineView = view.findViewById(R.id.listClassificationnineView)
+            listClassificationtenView = view.findViewById(R.id.listClassificationtenView)
+            listClassificationresultView = view.findViewById(R.id.listClassificationresultView)
         }
 
-        val done: Float = (classification.age.toFloat() - 24) / (89 - 24)
-        val dtwo = (classification.bmi - 18.37f) / (38.5787585f - 18.37f)
-        val dthree = (classification.glucose - 60) / (201 - 60)
-        val dfour = (classification.insulin - 2.432f) / (58.46f - 2.432f)
-        val dfive = (classification.homa - 4.311f) / (90.28f - 4.311f)
-        val dsix = (classification.leptin - 1.6502f) / (38.04f - 1.6502f)
-        val dseven = (classification.adiponectin - 3.21f) / (82.1f - 3.21f)
-        val deight = (classification.resistin - 45.843f) / (1698.44f - 45.843f)
-        val dnine = (classification.mcp - 45.843f) / (1698.44f - 45.843f)
-
-        val inputVal: FloatArray = floatArrayOf(done, dtwo, dthree, dfour, dfive, dsix, dseven,deight, dnine)
-        val outputVal: ByteBuffer = ByteBuffer.allocateDirect(8)
-        outputVal.order(ByteOrder.nativeOrder())
-
-        tflite.run(inputVal, outputVal)
-        outputVal.rewind()
-
-        val result = FloatArray(2)
-        for (i in 0..1) {
-            result[i] = outputVal.float
+        override fun toString(): String {
+            return super.toString() + " " + mItem
         }
-
-        if (result[0]>result[1])
-            res = "Result is negative"
-
-        else
-            res = "Result is positive"
-
-        classification.outcome = res
-        crudViewModel.editBreastCancer(BreastCancerVO( classification))
-
-        return res
-
     }
 }
